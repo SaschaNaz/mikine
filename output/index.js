dbd1e741ab1db54685bdf2b33cf05431c6fc4656
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-10-02 23:54:06 +0900

    ts watch mode / some basic query process try

--- output/index.js
+++ output/index.js
@@ -1,7 +1,27 @@
 "use strict";
 const http = require("http");
+const url = require("url");
+const pixivRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/member_illust.php/;
+const pixivShortRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/i\//;
 http.createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hoshii Miki, Chuu-san nano!\n");
+    const parsed = url.parse(request.url, true);
+    if (!parsed.query || !parsed.query.target) {
+        response.end("`target` parameter is required.\n");
+        return;
+    }
+    const target = parsed.query.target;
+    let output;
+    if (target.match(pixivRegex)) {
+        output = "pixiv";
+    }
+    else if (target.match(pixivShortRegex)) {
+        output = "pixivshort";
+    }
+    else {
+        response.end("Miki, kore shiranaino!");
+        return;
+    }
+    response.end(`Hoshii Miki, Chuu-san nano! Ah, ato ${output}`);
 }).listen(process.env.PORT);
 //# sourceMappingURL=index.js.map
--- sources/index.ts
+++ sources/index.ts
@@ -1,6 +1,27 @@
 import * as http from "http";
+import * as url from "url";
+
+const pixivRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/member_illust.php/;
+const pixivShortRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/i\//;
 
 http.createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hoshii Miki, Chuu-san nano!\n");
+    const parsed = url.parse(request.url, true);
+    if (!parsed.query || !parsed.query.target) {
+        response.end("`target` parameter is required.\n");
+        return;
+    }
+    const target = parsed.query.target as string;
+    let output: string;
+    if (target.match(pixivRegex)) {
+        output = "pixiv";
+    }
+    else if (target.match(pixivShortRegex)) {
+        output = "pixivshort"
+    }
+    else {
+        response.end("Miki, kore shiranaino!");
+        return;
+    }
+    response.end(`Hoshii Miki, Chuu-san nano! Ah, ato ${output}`);
 }).listen(process.env.PORT);

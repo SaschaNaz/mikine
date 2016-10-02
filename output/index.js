e87d963fef734d601845d7967abed80668aaf007
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-10-02 22:06:09 +0900

    wrong name :/

--- output/index.js
+++ output/index.js
@@ -4,4 +4,4 @@ http.createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hoshii Miki, Chuu-san nano!\n");
 }).listen(process.env.PORT);
-//# sourceMappingURL=input.js.map
+//# sourceMappingURL=index.js.map

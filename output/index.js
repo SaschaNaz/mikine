7b52d4cee29600760001d7d01db480e3ba23569f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-04-08 21:29:19 +0900

    use card-inal

--- output/index.js
+++ output/index.js
@@ -1,27 +1,75 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
-const url = require("url");
-const pixivRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/member_illust.php/;
-const pixivShortRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/i\//;
-http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    const parsed = url.parse(request.url, true);
-    if (!parsed.query || !parsed.query.target) {
-        response.end("`target` parameter is required.\n");
+const url_1 = require("url");
+const cardinal = require("card-inal");
+const node_fetch_1 = require("node-fetch");
+http.createServer((request, response) => __awaiter(this, void 0, void 0, function* () {
+    response.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+    const target = new url_1.URL(request.url, "http://localhost").searchParams.get("target");
+    if (!target) {
+        response.end(JSON.stringify({
+            message: "`target` parameter is required.",
+            errorType: "request"
+        }));
         return;
     }
-    const target = parsed.query.target;
-    let output;
-    if (target.match(pixivRegex)) {
-        output = "pixiv";
+    let url;
+    try {
+        url = new url_1.URL(target);
     }
-    else if (target.match(pixivShortRegex)) {
-        output = "pixivshort";
-    }
-    else {
-        response.end("Miki, kore shiranaino!");
+    catch (e) {
+        response.end(JSON.stringify({
+            message: "Incorrect `target` URL.",
+            errorType: "request"
+        }));
         return;
     }
-    response.end(`Hoshii Miki, Chuu-san nano! Ah, ato ${output}`);
-}).listen(process.env.PORT);
+    try {
+        const fetchResponse = yield node_fetch_1.default(target);
+        if (!fetchResponse.ok) {
+            response.end(JSON.stringify({
+                message: `Failed to fetch ${target}, ${fetchResponse.statusText}`,
+                errorType: "network",
+                httpCode: fetchResponse.status
+            }));
+            return;
+        }
+        let card;
+        try {
+            card = cardinal.parse(yield fetchResponse.text(), url.hostname);
+        }
+        catch (e) {
+            response.end(JSON.stringify({
+                message: e.message,
+                errorType: "card"
+            }));
+            return;
+        }
+        if (!card) {
+            response.end(JSON.stringify({
+                message: "No Twitter Card exists",
+                errorType: "normal",
+            }));
+        }
+        response.end(JSON.stringify({
+            data: card,
+            errorType: "normal"
+        }));
+    }
+    catch (e) {
+        response.end(JSON.stringify({
+            error: `Failed to fetch ${target} because of network issue`,
+            errorType: "network"
+        }));
+    }
+})).listen(process.env.PORT);
 //# sourceMappingURL=index.js.map
--- sources/index.ts
+++ sources/index.ts
@@ -1,27 +1,69 @@
 import * as http from "http";
-import * as url from "url";
+import { URLSearchParams, URL } from "url";
+import * as cardinal from "card-inal";
+import fetch from "node-fetch";
 
-const pixivRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/member_illust.php/;
-const pixivShortRegex = /^https?:\/\/(?:www\.)?pixiv\.net\/i\//;
-
-http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    const parsed = url.parse(request.url, true);
-    if (!parsed.query || !parsed.query.target) {
-        response.end("`target` parameter is required.\n");
+http.createServer(async (request, response) => {
+    response.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+    
+    const target = new URL(request.url, "http://localhost").searchParams.get("target");
+    if (!target) {
+        response.end(JSON.stringify({
+            message: "`target` parameter is required.",
+            errorType: "request"
+        }));
         return;
     }
-    const target = parsed.query.target as string;
-    let output: string;
-    if (target.match(pixivRegex)) {
-        output = "pixiv";
+
+    let url;
+    try {
+        url = new URL(target);
     }
-    else if (target.match(pixivShortRegex)) {
-        output = "pixivshort"
-    }
-    else {
-        response.end("Miki, kore shiranaino!");
+    catch (e) {
+        response.end(JSON.stringify({
+            message: "Incorrect `target` URL.",
+            errorType: "request"
+        }));
         return;
     }
-    response.end(`Hoshii Miki, Chuu-san nano! Ah, ato ${output}`);
+
+    try {
+        const fetchResponse = await fetch(target);
+        if (!fetchResponse.ok) {
+            response.end(JSON.stringify({
+                message: `Failed to fetch ${target}, ${fetchResponse.statusText}`,
+                errorType: "network",
+                httpCode: fetchResponse.status
+            }));
+            return;
+        }
+
+        let card
+        try {
+            card = cardinal.parse(await fetchResponse.text(), url.hostname);
+        }
+        catch (e) {
+            response.end(JSON.stringify({
+                message: e.message,
+                errorType: "card"
+            }));
+            return;
+        }
+        if (!card) {
+            response.end(JSON.stringify({
+                message: "No Twitter Card exists",
+                errorType: "normal",
+            }))
+        }
+        response.end(JSON.stringify({
+            data: card,
+            errorType: "normal"
+        }));
+    }
+    catch (e) {
+        response.end(JSON.stringify({
+            error: `Failed to fetch ${target} because of network issue`,
+            errorType: "network"
+        }))
+    }
 }).listen(process.env.PORT);
